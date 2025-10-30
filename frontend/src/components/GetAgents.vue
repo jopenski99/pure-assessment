5b8953e959146512f0d1e5e5b54f8b21ef6926a7
John Paul <jopenski99@gmail.com>
2025-10-30 20:20:23 +0800

    Init frontend and initial component

---
+++ frontend/src/components/GetAgents.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-4">
+    <h2 class="text-xl font-bold mb-4">Property Agents</h2>
+
+   
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+th,
+td {
+  text-align: left;
+}
+</style>
