ce551a926a28189891b46e54d0969a272391e024
John Paul <jopenski99@gmail.com>
2025-10-30 20:08:29 +0800

    added new agent POST

--- backend/src/routes/agent.ts
+++ backend/src/routes/agent.ts
@@ -10,11 +10,40 @@ interface PropertyAgent {
   updatedAt: Date;
 }
 
+let idCounter = 1;
+
 const router = express.Router();
 let agents: PropertyAgent[] = [];
 
 router.get("/", (req: Request, res: Response) => {
   res.json(agents);
+});
+
+router.post("/", (req: Request, res: Response) => {
+  const { firstName, lastName, email, mobileNumber } = req.body;
+
+  if (!firstName || !lastName || !email || !mobileNumber) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  const emailExists = agents.some(agent => agent.email === email);
+  if (emailExists) {
+    return res.status(400).json({ message: "Email already exists." });
+  }
+
+
+  const newAgent: PropertyAgent = {
+    id: idCounter++,
+    firstName,
+    lastName,
+    email,
+    mobileNumber,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  agents.push(newAgent);
+  return res.status(201).json(newAgent);
 });
 
 export default router;
